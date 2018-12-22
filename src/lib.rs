70cdf32e3138051d2dd6fb9f213211f2703fb1b1
r4nd0m <mk8100@yahoo.com>
2018-12-22 10:04:35 +0100

    add some Matrix code

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,59 @@ pub fn run(n:f64) -> RunResult {
 
     println!("result: {:?} {:?}", w, z);            
 
+    let v1:Vec<u32> = vec![2, 4, 8, 10];
+    let v2:Vec<u32> = vec![3, 5, 9, 11];
+
+    let r = v1.iter()
+        .zip(v2)
+        .map(|(a,b)| a*b)
+        .fold(0, |acc, a| acc +a);
+        //.collect::<Vec<u32>>();
+
+    println!("r = {:?}",r);
+
     match w {
         Some(_) => Ok( () ),
         None    => Err( ERR_MSG )
     }                
+
+}
+
+
+pub struct Matrix {
+    nrow: u32,
+    ncol: u32,
+    data: Vec<u32>,
+}
+
+use std::fmt::Display;
+use std::fmt;
+
+impl Display for Matrix{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cl = |(i,idx):(usize,&u32)| {if idx % self.ncol == 0 {i.to_string() + " |\n"}else{i.to_string()+", "}}; 
+        let w:String = self.data.iter()
+                        .enumerate()
+                        .map( cl )
+                        .collect::<Vec<String>>()
+                        .concat();
+        write!(f, "\n{}", w)
+    }
+}
+
+impl Matrix {
+        pub fn new(r:u32, c:u32, v: Vec<u32>)->Result<Matrix,&'static str>{
+            
+            if r*c != v.len() as u32 {
+                return Err("Not enough data inside vector")
+            }
+
+            Ok(Matrix{
+                nrow: r,
+                ncol: c,
+                data: v,
+            })
+        }
 
 }
 
